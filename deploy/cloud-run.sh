d192c29e9ccdfce44396f63f8898762b3577fc4a
Michael <mjthecoder53161@gmail.com>
2024-10-05 09:24:49 +0900

    updated cloud-run.sh script

--- deploy/cloud-run.sh
+++ deploy/cloud-run.sh
@@ -5,7 +5,6 @@ export PROJECT_ID=vegax-429008
 export REGION=asia-northeast3
 export IMAGE_URL="$REGION-docker.pkg.dev/$PROJECT_ID/file-drive/file-drive:v1.0"
 export SERVICE_ACCOUNT="compute-engine@$PROJECT_ID.iam.gserviceaccount.com"
-export DATABASE_URL='postgresql+psycopg2://admin:y7jHf&DNWG15@10.0.0.3:5030/main'
 
 # create a secret using secret manager for the database URL
 echo -n $DATABASE_URL | \
