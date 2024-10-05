e1d361e8149e265fa51fd70d66c58b8c6679abfa
Michael <mjthecoder53161@gmail.com>
2024-10-05 09:29:15 +0900

    updated cloud-run.sh script

--- deploy/cloud-run.sh
+++ deploy/cloud-run.sh
@@ -6,10 +6,6 @@ export REGION=asia-northeast3
 export IMAGE_URL="$REGION-docker.pkg.dev/$PROJECT_ID/file-drive/file-drive:v1.0"
 export SERVICE_ACCOUNT="compute-engine@$PROJECT_ID.iam.gserviceaccount.com"
 
-# create a secret using secret manager for the database URL
-echo -n $DATABASE_URL | \
-gcloud secrets versions add DATABASE_URL --data-file=-
-
 # Deploy Cloud Run Service
 gcloud run deploy demo-app \
   --image $IMAGE_URL \
