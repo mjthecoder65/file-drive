7ab42354e6cdb0d86ef0478740722a303ce00214
Michael <mjthecoder53161@gmail.com>
2024-10-05 09:23:40 +0900

    add mixins for timestamp

---
+++ models/mixins.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, DateTime, func
+
+
+class TimeStampMixin:
+    created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), nullable=False, default=func.now(), onupdate=func.now()
+    )
