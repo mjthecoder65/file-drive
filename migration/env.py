e7ccf2bcff9ed4dfcc0625932bb9f2f851f0dcdd
Michael <mjthecoder53161@gmail.com>
2024-10-05 10:15:01 +0900

    updated env.py: set main slqalchemy.url

--- migration/env.py
+++ migration/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from configs.database import Base
+from configs.settings import settings
 from models.user import *
 from models.file import *
 from models.insight import *
@@ -24,12 +26,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
 
 
 def run_migrations_offline() -> None:
