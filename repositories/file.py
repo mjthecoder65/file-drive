fb3f2e24175c90b83a2323a35e43f9408f1b8137
Michael <mjthecoder53161@gmail.com>
2024-10-06 16:37:10 +0900

    refactor: removed direct dependency on File model

--- repositories/file.py
+++ repositories/file.py
@@ -7,7 +7,6 @@ from models.file import File
 class FileRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
-        self.model = File
 
     async def add(self, file: File) -> File:
         self.db.add(file)
@@ -17,22 +16,20 @@ class FileRepository:
 
     async def get_by_user_id(self, user_id: str) -> list[File]:
         result = await self.db.execute(
-            select(self.model)
-            .filter_by(user_id=user_id)
-            .order_by(self.model.created_at.desc())
+            select(File).filter_by(user_id=user_id).order_by(File.created_at.desc())
         )
         return result.scalars().all()
 
     async def get_by_name(self, name: str) -> File:
-        result = await self.db.execute(select(self.model).filter_by(name=name))
+        result = await self.db.execute(select(File).filter_by(name=name))
         return result.scalars().first()
 
     async def get_by_id(self, id: str) -> File:
-        result = await self.db.execute(select(self.model).filter_by(id=id))
+        result = await self.db.execute(select(File).filter_by(id=id))
         return result.scalars().first()
 
     async def get_all(self) -> list[File]:
-        result = await self.db.execute(select(self.model))
+        result = await self.db.execute(select(File))
         return result.scalars().all()
 
     async def delete(self, file: File):
