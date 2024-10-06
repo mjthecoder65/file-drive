f8544461ce0c283263712a16aa2249603c8f68cb
Michael <mjthecoder53161@gmail.com>
2024-10-06 15:01:02 +0900

    updated file model

--- models/file.py
+++ models/file.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, ForeignKey, String
+import uuid
+
+from sqlalchemy import BigInteger, Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 
 from configs.database import Base
@@ -8,11 +10,11 @@ from models.mixins import TimeStampMixin
 class File(Base, TimeStampMixin):
     __tablename__ = "files"
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, index=True, nullable=False)
-    url = Column(String, nullable=False)
     extension = Column(String, nullable=False)
     mime_type = Column(String, nullable=False)
+    size = Column(BigInteger, nullable=False)
     user_id = Column(
         UUID(as_uuid=True),
         ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"),
