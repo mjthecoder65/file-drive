ffb3486c818aa29450950e00389351afbe7b7a56
Michael <mjthecoder53161@gmail.com>
2024-10-06 15:02:42 +0900

    updated file repo

--- repositories/file.py
+++ repositories/file.py
@@ -9,8 +9,18 @@ class FileRepository:
         self.db = db
         self.model = File
 
+    async def add(self, file: File) -> File:
+        self.db.add(file)
+        await self.db.commit()
+        await self.db.refresh(file)
+        return file
+
     async def get_by_user_id(self, user_id: str) -> list[File]:
-        result = await self.db.execute(select(self.model).filter_by(user_id=user_id))
+        result = await self.db.execute(
+            select(self.model)
+            .filter_by(user_id=user_id)
+            .order_by(self.model.created_at.desc())
+        )
         return result.scalars().all()
 
     async def get_by_name(self, name: str) -> File:
@@ -21,7 +31,10 @@ class FileRepository:
         result = await self.db.execute(select(self.model).filter_by(id=id))
         return result.scalars().first()
 
+    async def get_all(self) -> list[File]:
+        result = await self.db.execute(select(self.model))
+        return result.scalars().all()
+
     async def delete(self, file: File):
         self.db.delete(file)
         await self.db.commit()
-        return file
