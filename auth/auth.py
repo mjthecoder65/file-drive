c6701d9a9e886ec946ac372351662dd2249a6db0
Michael <mjthecoder53161@gmail.com>
2024-10-06 14:59:29 +0900

    updated annotation create_auth_token function

--- auth/auth.py
+++ auth/auth.py
@@ -53,3 +53,12 @@ async def get_current_user(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
         )
+
+
+async def only_admin_user(user: User = Depends(get_current_user)) -> bool:
+    if not user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don't have permission to access this resource.s",
+        )
+    return True
