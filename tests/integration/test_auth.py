69027940c8c91c81c5ca10030e77036e15a82da8
Michael <mjthecoder53161@gmail.com>
2024-10-10 11:39:21 +0900

    add more tests for auth endpoints

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -12,6 +12,7 @@ from configs.database import get_session
 from configs.settings import settings
 from main import app
 
+faker = Faker()
 engine = create_async_engine(settings.DATABASE_URL_TEST, echo=True)
 
 AsyncSessionLocal = sessionmaker(
@@ -57,3 +58,70 @@ class TestAuth:
         assert response.status_code == status.HTTP_201_CREATED
         assert response.json()["access_token"] is not None
         assert response.json()["token_type"] == "bearer"
+
+    async def test_register_existing_user(self, client: AsyncClient):
+        new_user = get_random_user()
+        payload = {
+            "username": new_user.username,
+            "email": new_user.email,
+            "password": new_user.password,
+        }
+
+        await client.post(f"{settings.API_ENDPOINT_PREFIX}/auth/register", json=payload)
+
+        response = await client.post(
+            f"{settings.API_ENDPOINT_PREFIX}/auth/register", json=payload
+        )
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json()["detail"] == "User with this email already exists"
+
+    async def test_login(self, client: AsyncClient):
+        new_user = get_random_user()
+        payload = {
+            "username": new_user.username,
+            "email": new_user.email,
+            "password": new_user.password,
+        }
+        await client.post(f"{settings.API_ENDPOINT_PREFIX}/auth/register", json=payload)
+
+        response = await client.post(
+            f"{settings.API_ENDPOINT_PREFIX}/auth/login",
+            data={"username": new_user.email, "password": new_user.password},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["access_token"] is not None
+        assert response.json()["token_type"] == "bearer"
+
+    async def test_login_wrong_email(self, client: AsyncClient):
+        new_user = get_random_user()
+        payload = {
+            "username": new_user.username,
+            "email": new_user.email,
+            "password": new_user.password,
+        }
+        await client.post(f"{settings.API_ENDPOINT_PREFIX}/auth/register", json=payload)
+
+        response = await client.post(
+            f"{settings.API_ENDPOINT_PREFIX}/auth/login",
+            data={"username": faker.email(), "password": new_user.password},
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json()["detail"] == "Wrong email or password"
+
+    async def test_login_wrong_password(self, client: AsyncClient):
+        new_user = get_random_user()
+        payload = {
+            "username": new_user.username,
+            "email": new_user.email,
+            "password": new_user.password,
+        }
+        await client.post(f"{settings.API_ENDPOINT_PREFIX}/auth/register", json=payload)
+
+        response = await client.post(
+            f"{settings.API_ENDPOINT_PREFIX}/auth/login",
+            data={"username": new_user.email, "password": faker.password()},
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json()["detail"] == "Wrong email or password"
