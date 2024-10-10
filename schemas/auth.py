1562e711bace66935ac1e57a37d39ea0e65b29c8
Michael <mjthecoder53161@gmail.com>
2024-10-10 10:59:47 +0900

    update pydantic model for RegisterPayloadModel

--- schemas/auth.py
+++ schemas/auth.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class RegisterPayloadModel(BaseModel):
     email: EmailStr
-    username: str = Field(..., min_length=6, max_length=50)
+    username: str = Field(..., min_length=2, max_length=50)
     password: str = Field(..., min_length=8, max_length=1024)
 
 
