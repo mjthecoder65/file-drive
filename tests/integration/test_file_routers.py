889c1f3d0fa805550af736545d77c34bab3c9767
Michael <mjthecoder53161@gmail.com>
2024-10-20 19:28:49 +0900

    update testcase for the endpoint for uploading file

--- tests/integration/test_file_routers.py
+++ tests/integration/test_file_routers.py
@@ -3,6 +3,7 @@ from unittest import mock
 
 import pytest
 from fastapi import status
+from faker import Faker
 from httpx import AsyncClient
 
 from configs.settings import settings
@@ -25,15 +26,17 @@ async def test_upload_file(client: AsyncClient):
     assert res.status_code == status.HTTP_201_CREATED
     assert res.json()["access_token"] is not None
 
+    faker = Faker()
     access_token = res.json()["access_token"]
     file_stream = io.BytesIO(b"Sample file data")
-    file_name = "sample.txt"
+    file_name = faker.name()
     file = {"file": (file_name, file_stream, "text/plain")}
 
+    fake_signed_url = faker.url(schemes=["https"])
     with mock.patch.object(
         FileService, "_upload_to_gcs", return_value=None
     ) as _upload_to_gcs, mock.patch.object(
-        FileService, "_generate_signed_url", return_value="http://mockurl.com"
+        FileService, "_generate_signed_url", return_value=fake_signed_url
     ) as _generate_signed_url:
         res = await client.post(
             f"{settings.API_ENDPOINT_PREFIX}/files",
@@ -45,4 +48,4 @@ async def test_upload_file(client: AsyncClient):
         assert _upload_to_gcs.called
         assert _generate_signed_url.called
         assert file_name in res.json()["name"]
-        assert "http://mockurl.com" in res.json()["url"]
+        assert fake_signed_url in res.json()["url"]
