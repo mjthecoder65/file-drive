4b98ac6af9e12a9273b8353cd49c18bce4966389
Michael <mjthecoder53161@gmail.com>
2024-10-21 10:48:56 +0900

    updated pagination base model

--- schemas/pagination.py
+++ schemas/pagination.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, Field
 class PaginationBaseModel(BaseModel):
     limit: int = Field(10, ge=1)
     offset: int = Field(0, ge=0)
-    total: int
+    total: int = Field(..., ge=0)
